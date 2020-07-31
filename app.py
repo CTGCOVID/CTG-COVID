ad4b1201259fbea9d3cb8b8a61d9673d85979e2d
CTGCOVID <68571542+CTGCOVID@users.noreply.github.com>
2020-07-31 11:55:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,63 +4,115 @@ import json
 with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
     counties = json.load(response)
 import os
+import plotly.express as px
+from datetime import datetime
 from datetime import date
 from datetime import timedelta
-import plotly.express as px
-
-
-FIPS_pd = pd.read_csv('https://raw.githubusercontent.com/CTGCOVID/CTG-COVID/master/FIPS.csv')
-States_pd = pd.read_csv('https://raw.githubusercontent.com/CTGCOVID/CTG-COVID/master/States.csv')
-CombinedFIPS_pd = pd.read_csv('https://raw.githubusercontent.com/CTGCOVID/CTG-COVID/master/CombinedFIPS.csv')
-CombinedTotal_pd = pd.read_csv('https://raw.githubusercontent.com/CTGCOVID/CTG-COVID/master/CombinedTotal.csv')
-
-
-
-FIPS_pd['FIPS'] = FIPS_pd['FIPS'].astype('int64', copy=True)
-
-strFIPS=[]
-
-for x in range(0,len(FIPS_pd)):
-    strFIPS.append(str(FIPS_pd['FIPS'][x]).rjust(5,'0'))
-FIPS_pd['FIPS'] = strFIPS
-
-
-mean = FIPS_pd['Incidence_Rate'].mean()
-std = FIPS_pd['Incidence_Rate'].std()
-
-colorscale = ["#CCFFCC","#00FF00","#99FF00","#CCFF00","#FFFF00","#FFCC00","#FF6600","#FF0000"]
-
-fig = px.choropleth(FIPS_pd, geojson=counties, locations='FIPS', color= 'Incidence_Rate',
-                    color_continuous_scale=colorscale,
-                    range_color=(0, mean+std*2), 
-                    scope="usa", title='Active Incidence Rate per County', hover_name = "Combined_Key", hover_data=['Risk'])
-
-
-fig.update_traces(marker_line_width=.3, marker_opacity=.8,hovertemplate='<b>%{hovertext}</b><br>Risk: %{customdata[0]}<br><br>Incidence Rate: %{z}<br>Active Cases: %{customdata[1]}<extra></extra>')
-fig.update_layout(height=700, legend = dict(x=0.8),title_x = 0.4, font={"size":20, "color":"white"},geo=dict(bgcolor='#323130', lakecolor='#323130', subunitcolor='black'), plot_bgcolor='#111110', paper_bgcolor='#111110')
-fig.update_geos(showsubunits=True, subunitcolor='black')
-
-fig2 = px.choropleth(States_pd, locations='State_Code', color= 'DOD_Rank',
-                    scope = "usa",hover_name = "Province_State", locationmode='USA-states', color_discrete_sequence = ['#4CBB17','red'], title='DOD State Rankings')
-
-fig2.update_layout(height=700, legend = dict(x=0.8),title_x = 0.4, font={"size":20, "color":"white"},geo=dict(bgcolor='#323130', lakecolor='#323130', 
-                    subunitcolor='black'), plot_bgcolor='#111110', paper_bgcolor='#111110')
-########################################################################################################################################
-
-
-
-
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+from statistics import mean
+from statistics import stdev
 import math
 
-states = States_pd['Province_State'].unique()
+url = 'https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_confirmed_usafacts.csv'
+popUrl = 'https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_county_population_usafacts.csv'
+deathUrl = 'https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_deaths_usafacts.csv'
+
+confirmed_pd = pd.read_csv(url, index_col=False)
+population_pd = pd.read_csv(popUrl, index_col=False)
+deaths_pd = pd.read_csv(deathUrl, index_col=False)
+
+columns = confirmed_pd.columns
+today = datetime.strptime(columns[-1],'%m/%d/%y').date()
+
+confirmed_pd['Population'] = population_pd['population']
+deaths_pd['Population'] = population_pd['population']
+
+confirmed_pd = confirmed_pd[confirmed_pd['Population']!=0].reset_index(drop=True)
+deaths_pd = deaths_pd[deaths_pd['Population']!=0].reset_index(drop=True)
+
+y_vals=[]
+ranges=[]
+
+sunday = today
+day = sunday.weekday()
+
+while(day != 6):
+    sunday = sunday - timedelta(days=1)
+    day = sunday.weekday()
+
+TE = sunday
+TE = TE.strftime("%#m/%#d/%y")
+TS = sunday - timedelta(days=7)
+TS = TS.strftime("%#m/%#d/%y")
+LE = sunday - timedelta(days=7)
+LE = LE.strftime("%#m/%#d/%y")
+LS = sunday - timedelta(days=14)
+LS = LS.strftime("%#m/%#d/%y")
+
+risk = []
+graphIR = []
+
+for x in range(0,len(confirmed_pd)):
+    graphIR.append((confirmed_pd[TE][x]-confirmed_pd[TS][x])/confirmed_pd['Population'][x]*100000)
+
+mean = mean(graphIR)
+std = stdev(graphIR)
+x_vals=[mean-std*3]
+
+for x in range(1,61):
+    x_vals.append(x_vals[x-1]+std/10)
+
+for x in range(0,len(x_vals)):
+    y_vals.append((1/(std*math.sqrt(2*math.pi)))*math.exp(-1/2*((x_vals[x]-mean)/(std))**2))
+
+for x in range(0,len(x_vals)):
+    if x_vals[x]<0:
+        ranges.append('Out')
+    elif x<40:
+        ranges.append('Low')
+    elif x>=40 and x<51:
+        ranges.append('Medium')
+    else:
+        ranges.append('High')
+
+Curve = pd.DataFrame(list(zip(x_vals, y_vals, ranges)), columns = ['X', 'Y', 'Range']) 
+   
+########################################################################################################################################
+
+
+
+########################################################################################################################################
+#Import COVID DataZ
+########################################################################################################################################
+
+
+########################################################################################################################################
+
+
+states = confirmed_pd['State'].unique()
 states.sort()
-counties = CombinedFIPS_pd['Admin2'].unique()
+counties = confirmed_pd['County Name'].unique()
 counties.sort()
+
+
+
+
+
+States_pd = pd.DataFrame(list(zip(states)), columns = ['State'])
+States_pd['Color'] = 'Green'
+States_pd.loc[States_pd['State'] == 'FL','Color'] = 'Red'
+    
+fig2 = px.choropleth(States_pd, locations='State', color= 'Color',
+            scope = "usa",hover_name = "State", locationmode='USA-states', color_discrete_sequence = ['#4CBB17','red'], title='DOD State Rankings')
+
+fig2.update_layout(height=700, legend = dict(x=0.8),title_x = 0.4, font={"size":20, "color":"white"},geo=dict(bgcolor='#323130', lakecolor='#323130', 
+                subunitcolor='black'), plot_bgcolor='#111110', paper_bgcolor='#111110')
+
+
+
 
 app = dash.Dash(__name__)
 server=app.server
@@ -69,21 +121,10 @@ server=app.server
 app.layout = html.Div([
     html.Div([
         html.Div([
-            dcc.Graph(id = 'g1',figure=fig),
-        ],className='ten columns', style={'width':'60%'}),
-        html.Div([
-            dcc.Graph(id='g2',figure=fig2),
-        ],className='two columns', style={'width':'40%'}),
-    ],className='row', style={'display':'flex'}),
-    
-    html.Br(),
-    html.Br(),
-    html.Div([
-        html.Div([
             html.Div(children='State: '),
-            dcc.Dropdown(id = 'state_dropdown', options=[{'label' : s, 'value' : s} for s in states], value='Florida', style={'height':'50px', 'width':'90%', 'font-size':'120%'}),
+            dcc.Dropdown(id = 'state_dropdown', options=[{'label' : s, 'value' : s} for s in states], value='FL', style={'height':'50px', 'width':'90%', 'font-size':'120%'}),
             html.Div(children='County: ',style={'font-color':'white'}),
-            dcc.Dropdown(id = 'county_dropdown', options=[{'label' : c, 'value' : c} for c in counties], value='All', style={'height':'50px', 'width':'90%', 'font-size':'120%'}),
+            dcc.Dropdown(id = 'county_dropdown', options=[{'label' : c, 'value' : c} for c in counties], value='Okaloosa County', style={'height':'50px', 'width':'90%', 'font-size':'120%'}),
             html.Br(),
             html.Br(),
             dcc.Graph(id='riskgraph'),
@@ -107,31 +148,35 @@ app.layout = html.Div([
     [Input(component_id='state_dropdown', component_property='value'), Input(component_id='county_dropdown', component_property='value')]
 )
 def graph_active(state_slctd, county_slctd):
-    local = CombinedFIPS_pd.copy()
-    local = local[local["Admin2"]=='Okaloosa'].reset_index(drop=True)
-    dff = CombinedFIPS_pd.copy()
-    dff = dff[dff["Province_State"] == state_slctd]
-    dff = dff[dff["Admin2"] == county_slctd]
+    dff = confirmed_pd.copy()
+    dff = dff[dff["State"] == state_slctd]
+    dff = dff[dff["County Name"] == county_slctd]
     dff = dff.reset_index(drop=True)
+    dffdeaths = deaths_pd.copy()
+    dffdeaths = dffdeaths[dffdeaths['State'] == state_slctd]
+    dffdeaths = dffdeaths[dffdeaths['County Name'] == county_slctd]
+    dffdeaths = dffdeaths.reset_index(drop=True)
 
     x=[]
     y=[]
-    z=[]
-    i=61
 
-    while i > 0:
-        day = date(2020,7,26) - timedelta(days=i)
-        di = day.strftime("%m-%d-%Y")
-        x.append(di)
-        y.append(dff[di][0])
-        i-=1
-    
+    i=61
+    while i>0:
+        day1 = today - timedelta(days=i)
+        d1 = day1.strftime("%#m/%#d/%y")
+        day2 = day1 - timedelta(days=14)
+        d2 = day2.strftime("%#m/%#d/%y")
+
+        x.append(d1)
+        y.append(dff[d1][0] - dff[d2][0] - dffdeaths[d1][0])
+        i -= 1
+
     fig3 = px.bar(x=x, y=y, title='Active Cases')
     fig3.update_traces(marker_color='#00ff00')
     fig3.update_xaxes(showline=True, linecolor='white', title_text='Date')
     fig3.update_yaxes(showline=True, linecolor='white', title_text='Active Cases')
     fig3.update_layout(height=300,yaxis_showgrid=False, xaxis_tickangle = -45, title_x = 0.4, font={"size":15, "color":"gray"}, plot_bgcolor='#111110', paper_bgcolor='#111110', title_font_color='white')
-    
+
     return fig3
 
 
@@ -139,28 +184,46 @@ def graph_active(state_slctd, county_slctd):
     Output(component_id='IRgraph', component_property='figure'),
     [Input(component_id='state_dropdown', component_property='value'), Input(component_id='county_dropdown', component_property='value')]
 )
-def graph_active(state_slctd, county_slctd):
-    local = CombinedFIPS_pd.copy()
-    local = local[local["Admin2"]=='Okaloosa'].reset_index(drop=True)
-    dff = CombinedFIPS_pd.copy()
-    dff = dff[dff["Province_State"] == state_slctd]
-    dff = dff[dff["Admin2"] == county_slctd]
-    dff = dff.reset_index(drop=True)
+def graph_IR(state_slctd, county_slctd):
+    local = confirmed_pd.copy()
+    local = local[local["State"]=='FL']
+    local1 = local[local["County Name"]=='Okaloosa County'].reset_index(drop=True)
+    local2 = local[local["County Name"]=='Walton County'].reset_index(drop=True)
+    local3 = local[local["County Name"]=='Santa Rosa County'].reset_index(drop=True)
+    dff = confirmed_pd.copy()
+    dff = dff[dff["State"] == state_slctd]
+    dff = dff[dff["County Name"] == county_slctd].reset_index(drop=True)
+    localdeaths = deaths_pd.copy()
+    localdeaths = localdeaths[localdeaths['State']=='FL']
+    localdeaths1 = localdeaths[localdeaths['County Name']=='Okaloosa County'].reset_index(drop=True)
+    localdeaths2 = localdeaths[localdeaths['County Name']=='Walton County'].reset_index(drop=True)
+    localdeaths3 = localdeaths[localdeaths['County Name']=='Santa Rosa County'].reset_index(drop=True)
+    dffdeaths = deaths_pd.copy()
+    dffdeaths = dffdeaths[dffdeaths['State'] == state_slctd]
+    dffdeaths = dffdeaths[dffdeaths['County Name'] == county_slctd].reset_index(drop=True)
+
+    localPop = local1['Population'][0] + local2['Population'][0] + local3['Population'][0]
 
     x=[]
+    y=[]
     z=[]
-    z2 = []
-    i=61
 
-    while i > 0:
-        day = date(2020,7,26) - timedelta(days=i)
-        di = day.strftime("%m-%d-%Y")
-        x.append(di)
-        z.append(dff[di][0]/dff['Population'][0]*100000)
-        z2.append(local[di][0]/local['Population'][0]*100000)
-        i-=1
-    
-    LineData = pd.DataFrame(list(zip(x,z,z2)), columns = ['Dates', county_slctd, 'Local']) 
+    i=61
+    while i>0:
+        day1 = today - timedelta(days=i)
+        d1 = day1.strftime("%#m/%#d/%y")
+        day2 = day1 - timedelta(days=14)
+        d2 = day2.strftime("%#m/%#d/%y")
+
+        x.append(d1)
+        localsum = local1[d1][0] + local2[d1][0] + local3[d1][0]
+        local14sum = local1[d2][0] + local2[d2][0] + local3[d2][0]
+        localdeathsum = localdeaths1[d1][0] + localdeaths2[d1][0] + localdeaths3[d1][0]
+        y.append((localsum - local14sum - localdeathsum)/localPop*100000)
+        z.append((dff[d1][0] - dff[d2][0] - dffdeaths[d1][0])/dff['Population'][0]*100000)
+        i -= 1
+
+    LineData = pd.DataFrame(list(zip(x,z,y)), columns = ['Dates', county_slctd, 'Local']) 
     
     fig4 = px.line(LineData, x='Dates', y=[county_slctd, 'Local'], title='Incidence Rate')
     fig4.update_xaxes(title_text='Date')
@@ -174,27 +237,30 @@ def graph_active(state_slctd, county_slctd):
     Output(component_id='newgraph', component_property='figure'),
     [Input(component_id='state_dropdown', component_property='value'), Input(component_id='county_dropdown', component_property='value')]
 )
-def graph_active(state_slctd, county_slctd):
-    total = CombinedTotal_pd.copy()
-    total = total[total['Province_State']== state_slctd]
-    total = total[total['Admin2']==county_slctd]
-    total = total.reset_index(drop=True)
+def graph_new(state_slctd, county_slctd):
+    dff = confirmed_pd.copy()
+    dff = dff[dff["State"] == state_slctd]
+    dff = dff[dff["County Name"] == county_slctd]
+    dff = dff.reset_index(drop=True)
+    dffdeaths = deaths_pd.copy()
+    dffdeaths = dffdeaths[dffdeaths['State'] == state_slctd]
+    dffdeaths = dffdeaths[dffdeaths['County Name'] == county_slctd].reset_index(drop=True)
 
     x=[]
-    z3=[]
-    i=61
-
-    while i > 0:
-        day = date(2020,7,26) - timedelta(days=i)
-        di = day.strftime("%m-%d-%Y")
-        day2 = date(2020,7,26) - timedelta(days=(i+1))
-        di2 = day2.strftime("%m-%d-%Y")
-        x.append(di)
-        difference = abs(total[di][0]-total[di2][0])
-        z3.append(difference)
-        i-=1
+    y=[]
     
-    fig5 = px.bar(x=x, y=z3, title='New Cases')
+    i=61
+    while i>0:
+        day1 = today - timedelta(days=i)
+        d1 = day1.strftime("%#m/%#d/%y")
+        day2 = day1 - timedelta(days=1)
+        d2 = day2.strftime("%#m/%#d/%y")
+
+        x.append(d1)
+        y.append(dff[d1][0] - dff[d2][0])
+        i -= 1
+    
+    fig5 = px.bar(x=x, y=y, title='New Cases')
     fig5.update_traces(marker_color='#00ff00')
     fig5.update_xaxes(showline=True, linecolor='white', title_text='Date')
     fig5.update_yaxes(showline=True, linecolor='white', title_text='New Cases')
@@ -207,44 +273,28 @@ def graph_active(state_slctd, county_slctd):
     Output(component_id='riskgraph', component_property='figure'),
     [Input(component_id='state_dropdown', component_property='value'), Input(component_id='county_dropdown', component_property='value')]
 )
-def graph_risk(state_slctd, county_slctd):
-    
-    x_vals=[mean-std*3]
-    y_vals=[]
-    ranges=[]
-    
-    point = CombinedFIPS_pd.copy()
-    point = point[point["Province_State"] == state_slctd]
-    point = point[point["Admin2"] == county_slctd]
+def graph_new(state_slctd, county_slctd):
+    point = confirmed_pd.copy()
+    point = point[point["State"] == state_slctd]
+    point = point[point["County Name"] == county_slctd]
     point = point.reset_index(drop=True)
- 
-    
-    for x in range(1,61):
-        x_vals.append(x_vals[x-1]+std/10)
 
-    for x in range(0,len(x_vals)):
-        y_vals.append((1/(std*math.sqrt(2*math.pi)))*math.exp(-1/2*((x_vals[x]-mean)/(std))**2))
-    
-    for x in range(0,len(x_vals)):
-        if x_vals[x]<0:
-            ranges.append('Out')
-        elif x<40:
-            ranges.append('Low')
-        elif x>=40 and x<51:
-            ranges.append('Medium')
-        else:
-            ranges.append('High')
-    
-    Curve = pd.DataFrame(list(zip(x_vals, y_vals, ranges)), columns = ['X', 'Y', 'Range'])   
+    thisIR = (point[TE][0] - point[TS][0])/point['Population'][0]*100000
+    lastIR = (point[LE][0] - point[LS][0])/point['Population'][0]*100000
 
+    if thisIR > lastIR:
+        highIR = thisIR
+    else:
+        highIR = lastIR
+    
     fig6 = px.area(Curve, x='X', y='Y', title = 'Risk Level', color='Range', color_discrete_sequence=['#C0C0C0', '#00FF00', '#FFFF00', '#FF0000'])
     fig6.update_traces(marker_color='#00ff00')
     fig6.update_xaxes(showline=True, linecolor='white', title_text='Date')
     fig6.update_yaxes(showline=True, linecolor='white', title_text='New Cases')
-    fig6.update_layout(annotations=[dict(x=point['Incidence_Rate'][0], y=0, showarrow=True, arrowhead=2, arrowwidth=3, arrowcolor='white', ax = 0, ay = -80, )], height=500,yaxis_showgrid=False, xaxis_showgrid=False, xaxis_tickangle = -45, title_x = 0.4, font={"size":15, "color":"gray"}, plot_bgcolor='#111110', paper_bgcolor='#111110', title_font_color='white')
-    
+    fig6.update_layout(annotations=[dict(x=highIR, y=0, showarrow=True, arrowhead=2, arrowwidth=3, arrowcolor='white', ax = 0, ay = -80, )], height=500,yaxis_showgrid=False, xaxis_showgrid=False, xaxis_tickangle = -45, title_x = 0.4, font={"size":15, "color":"gray"}, plot_bgcolor='#111110', paper_bgcolor='#111110', title_font_color='white')
+
     return fig6
 
 if __name__ == '__main__':
     app.run_server(debug=False)
-# ------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
